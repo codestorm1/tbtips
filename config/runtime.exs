ae73f256c0528a0c192075b756f41a8fe9d18ecd
Bryan Green <twistedogre@gmail.com>
2025-03-27 01:45:24 -0700

    Format fix

--- config/runtime.exs
+++ config/runtime.exs
@@ -65,13 +65,10 @@ if config_env() == :prod do
     ],
     secret_key_base: secret_key_base
 
-    # claude.ai
-    config :tbtips, Tbtips.Repo,
+  # claude.ai
+  config :tbtips, Tbtips.Repo,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-
-
-
 
   # ## SSL Support
   #
